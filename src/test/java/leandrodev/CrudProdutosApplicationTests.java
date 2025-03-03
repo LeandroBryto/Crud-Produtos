ca73123b56983a9e112bcedfd76dc680ced5a1ea
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-03-03 18:20:02 -0300

    estrutura inicial do projeto

---
+++ src/test/java/leandrodev/CrudProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package leandrodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
