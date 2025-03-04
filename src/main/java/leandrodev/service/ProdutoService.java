5b22a5b8877b35f0858a9214bf27c33696747a77
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-03-03 21:05:45 -0300

    Adiciona testes unitários
    Adiciona testes unitários para ProdutoService

--- src/main/java/leandrodev/service/ProdutoService.java
+++ src/main/java/leandrodev/service/ProdutoService.java
@@ -2,6 +2,8 @@ package leandrodev.service;
 
 import leandrodev.models.Produto;
 import leandrodev.repository.ProdutoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,22 +13,48 @@ import java.util.Optional;
 @Service
 public class ProdutoService {
 
+    // Logger para a classe ProdutoService
+    private static final Logger logger = LoggerFactory.getLogger(ProdutoService.class);
+
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    // Listando todos os produtos
     public List<Produto> listarTodos() {
-        return produtoRepository.findAll();
+        logger.info("Listando todos os produtos");
+        List<Produto> produtos = produtoRepository.findAll();
+        logger.info("Total de produtos encontrados: {}", produtos.size());
+        return produtos;
     }
 
+    // Buscando produto por ID
     public Optional<Produto> buscarPorId(Long id) {
-        return produtoRepository.findById(id);
+        logger.info("Buscando produto com ID: {}", id);
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if (produto.isPresent()) {
+            logger.info("Produto encontrado: {}", produto.get().getNome());
+        } else {
+            logger.warn("Produto com ID {} não encontrado", id);
+        }
+        return produto;
     }
 
+    // Salvando produto
     public Produto salvar(Produto produto) {
-        return produtoRepository.save(produto);
+        logger.info("Salvando produto: {}", produto.getNome());
+        Produto savedProduto = produtoRepository.save(produto);
+        logger.info("Produto salvo com sucesso: {}", savedProduto.getNome());
+        return savedProduto;
     }
 
+    // Excluindo produto
     public void excluir(Long id) {
-        produtoRepository.deleteById(id);
+        logger.info("Excluindo produto com ID: {}", id);
+        try {
+            produtoRepository.deleteById(id);
+            logger.info("Produto com ID {} excluído com sucesso", id);
+        } catch (Exception e) {
+            logger.error("Erro ao excluir produto com ID {}: {}", id, e.getMessage());
+        }
     }
 }
